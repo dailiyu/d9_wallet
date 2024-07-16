cdbf8905fbe9d4827e7c1715e8101bb23bc9db24
Chen-Wanying <417307918@qq.com>
2024-07-16 15:59:49 +0800

    1

--- src/views/home.vue
+++ src/views/home.vue
@@ -27,6 +27,22 @@
             Digital Assets
           </div>
         </div>
+        <div class="wallet_detail">
+          <div class="wallet_text">
+            <div class="text_left">
+              <div>钱包余额</div>
+              <img src="@/assets\home/view-fill.png" alt="" class="view_pic">
+            </div>
+            <div class="text_right">
+              <div>钱包管理</div>
+              <img src="@/assets/home/arrow-right.png" alt="" class="arrow_pic">
+            </div>
+          </div>
+          <div class="wallet_balance">
+            <div class="balance_symbol">$</div>
+            <div class="balance_num">0.00</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -79,6 +95,7 @@ import { IonPage } from '@ionic/vue';
   .swiper {
     padding: 0 8.6449vw;
     .swiper_item {
+      position: relative;
       width: 82.9439vw;
       height: 128.7383vw;
       border-radius: 25px;
@@ -101,8 +118,56 @@ import { IonPage } from '@ionic/vue';
           border-radius: 9px;
           background-color: #0065B2;
           color: #fff;
-          font-size: 1.8692vw;
+          font-size: 1.692vw;
           text-align: center;
+        }
+      }
+      .wallet_detail {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: #fff;
+        padding: 7.0093vw 4.6729vw;
+        border-radius: 25px;
+        .wallet_text {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          .text_left {
+            font-size: 4.2056vw;
+            font-weight: bold;
+            display: flex;
+            align-items: center;
+            .view_pic {
+              width: 3.5047vw;
+              height: auto;
+              margin-left: 2.3364vw;
+            }
+          }
+          .text_right {
+            display: flex;
+            align-items: center;
+            color: #0065FF;
+            font-size: 2.8037vw;
+            .arrow_pic {
+              width: 1.1682vw;
+              height: auto;
+              margin-left: 1.1682vw;
+            }
+          }
+        }
+        .wallet_balance {
+          display: flex;
+          align-items: baseline;
+          font-weight: bold;
+          padding: 4.6729vw 0;
+          .balance_symbol {
+            font-size: 4.2056vw;
+          }
+          .balance_num {
+            font-size: 8.4112vw;
+          }
         }
       }
     }
--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -3,22 +3,18 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1" :selected="tab1Active">
-          <!-- <ion-icon aria-hidden="true" :icon="triangle" /> -->
-           <img src="@/assets/tabs/home-fill.png" alt="" class="tabs_pic" v-show="tab1Active">
-           <!-- <img src="@/assets/tabs/home.png" alt="" class="tabs_pic" v-show="!tab1Active"> -->
+        <ion-tab-button tab="tab1" href="/main/home">
+          <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <!-- <ion-icon aria-hidden="true" :icon="ellipse" /> -->
-          <img src="@/assets/tabs/all-fill.png" alt="" class="tabs_pic">
+        <ion-tab-button tab="tab2" href="/main/discovery">
+          <ion-icon aria-hidden="true" :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <!-- <ion-icon aria-hidden="true" :icon="square" /> -->
-          <img src="@/assets/tabs/customer-fill.png" alt="" class="tabs_pic">
+        <ion-tab-button tab="tab3" href="/main/myself">
+          <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -29,8 +25,6 @@
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import { ref } from 'vue';
-const tab1Active = ref(true)
 </script>
 
 <style lang="scss" scoped>
