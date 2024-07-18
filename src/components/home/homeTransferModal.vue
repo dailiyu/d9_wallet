5aab063cd0a32f6ec3ea99b6222f17286b551b7e
Chen-Wanying <417307918@qq.com>
2024-07-18 15:44:05 +0800

    1

---
+++ src/components/home/homeTransferModal.vue
@@ -0,0 +1,186 @@
+<template>
+  <!-- <ion-header>
+    <ion-toolbar>
+      <ion-title>App</ion-title>
+    </ion-toolbar>
+  </ion-header> -->
+    <!-- <ion-button id="openTransferModal" expand="block">Open Sheet Modal</ion-button> -->
+
+    <ion-modal trigger="openTransferModal" :initial-breakpoint="1" :breakpoints="[0, 1]">
+      <div class="transfer_modal">
+        <div class="transfer_title">
+            <div>接收账户</div>
+            <img src="@/assets/home/address-book-fill.png" alt="" class="account_pic">
+        </div>
+        <div class="transfer_item">
+            <ion-input style="min-height: 10px;font-size: 2.8037vw;" placeholder="输入或粘贴接收账户地址">
+            <img src="@/assets/home/scan-grey.png" alt="" class="scan_pic"  slot="end">
+            </ion-input>
+        </div>
+        <div class="transfer_title">
+            <div>转账数量</div>
+        </div>
+        <div class="transfer_item">
+          <ion-input style="min-height: 10px;font-size: 7.0093vw;border-bottom: 1px solid #E7EBF2;padding-bottom: 2.8037vw;" placeholder="输入转账数量">
+            <div class="unit" slot="end">
+              <div>D9</div>
+              <img src="@/assets/home/arrow-right.png" alt="" class="arrow_pic" >
+            </div>
+          </ion-input>
+
+          <div class="balance">
+            <div class="balance_text">余额</div>
+            <div class="balance_num">156.6192</div>
+          </div>
+        </div>
+
+        <div class="transfer_title">
+            <div>网络费</div>
+        </div>
+        <div class="transfer_item">
+          <div class="estimate_fee">
+            <div class="fee_text">预计网络费用</div>
+            <div class="fee_num">
+              <img src="@/assets/home/square_d9.png" alt="" class="logo">
+              <div class="fee_unit">D9</div>
+              <div>7.00</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="confirm_btn">确认</div>
+
+        <div class="personal_info">
+          <img src="@/assets/home/square_d9.png" alt="" class="logo">
+          <div>Person (3842)</div>
+        </div>
+
+      </div>
+    </ion-modal>
+</template>
+
+<script lang="ts" setup>
+  import { IonButton, IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonIcon } from '@ionic/vue';
+</script>
+
+<style scoped lang="scss">
+ion-modal {
+  --height: 140.1869vw;
+  --border-radius: 25px 25px 0 0;
+  --backdrop-opacity: 1;
+}
+ion-modal::part(backdrop) {
+    background: #0065B2;
+    opacity: 1;
+}
+.transfer_modal {
+    padding: 16.3551vw 0;
+    overflow-y: scroll;
+    .transfer_title {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 3.5047vw;
+        padding: 0 10.0467vw;
+        font-weight: 500;
+        margin-bottom: 3.972vw;
+        .account_pic {
+            width: 4.6729vw;
+            height: auto;
+        }
+    }
+    .transfer_item {
+        padding: 2.8037vw 5.1402vw;
+        border-radius: 13px;
+        border: 1px solid #E7EBF2;
+        margin: 0 4.9065vw 6.5421vw;
+        .scan_pic {
+            width: 4.6729vw;
+            height: 4.6729vw;
+        }
+        ion-input {
+            // --padding-bottom: 0;
+            // --padding-end: 0;
+            // --padding-start: 0;
+            // --padding-top: 0;
+            --placeholder-color: #8E8C8E;
+            --highlight-color-focused: #000;
+        }
+        ion-button {
+            margin: 0;
+            --padding-bottom: 0;
+            --padding-end: 0;
+            --padding-start: 0;
+            --padding-top: 0;
+        }
+        .unit {
+          display: flex;
+          align-items: center;
+          color: #0065FF;
+          font-size: 2.8037vw;
+          font-weight: 500;
+          .arrow_pic {
+            width: 1.1682vw;
+            height: auto;
+            margin-left: 1.1682vw;
+          }
+        }
+        .balance {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          color: #8E8C8E;
+          padding-top: 3.271vw;
+          .balance_text {
+            font-size: 3.5047vw;
+          }
+          .balance_num {
+            font-size: 4.9065vw;
+          }
+        }
+        .estimate_fee {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          font-size: 2.8037vw;
+          .fee_text {
+            color: #8E8C8E;
+          }
+          .fee_num {
+            display: flex;
+            align-items: center;
+            .logo {
+              width: 5.1402vw;
+              height: auto;
+            }
+            .fee_unit {
+              padding: 0 2.8037vw 0 1.1682vw;
+            }
+          }
+        }
+    }
+    .confirm_btn {
+      width: 33.4112vw;
+      height: 12.6168vw;
+      line-height: 12.6168vw;
+      border-radius: 9px;
+      background-color: #0065FF;
+      color: #fff;
+      text-align: center;
+      margin: 22.4299vw auto 17.0561vw;
+    }
+    .personal_info {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 4.2056vw;
+      font-weight: 500;
+      .logo {
+        width: 5.6075vw;
+        height: auto;
+        margin-right: .9346vw;
+      }
+    }
+}
+</style>
+
--- src/views/home.vue
+++ src/views/home.vue
@@ -3,7 +3,7 @@
     <div class="main">
     <div class="main_top">
       <div>
-        <div class="name">Person</div>
+        <div class="name" id="openTransferModal">Person</div>
         <div class="account">DAUS1281******SAD3842</div>
       </div>
       <div class="functions">
@@ -19,38 +19,16 @@
       </div>
     </div>
 
-    <div class="swiper">
-      <div class="swiper_item">
-        <div class="logos">
-          <img src="@/assets/home/d9-network.png" alt="" class="logo_d9">
-          <div class="logo_item">
-            Digital Assets
-          </div>
-        </div>
-        <div class="wallet_detail">
-          <div class="wallet_text">
-            <div class="text_left">
-              <div>钱包余额</div>
-              <img src="@/assets\home/view-fill.png" alt="" class="view_pic">
-            </div>
-            <div class="text_right">
-              <div>钱包管理</div>
-              <img src="@/assets/home/arrow-right.png" alt="" class="arrow_pic">
-            </div>
-          </div>
-          <div class="wallet_balance">
-            <div class="balance_symbol">$</div>
-            <div class="balance_num">0.00</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <homeSwiper></homeSwiper>
+    <homgTransferModal></homgTransferModal>
   </div>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage } from '@ionic/vue';
+import homeSwiper from '@/components/home/homeSwiper.vue';
+import homgTransferModal from '@/components/home/homeTransferModal.vue'
 </script>
 <style lang="scss" scoped>
 .main {
@@ -92,85 +70,6 @@ import { IonPage } from '@ionic/vue';
     }
   }
 
-  .swiper {
-    padding: 0 8.6449vw;
-    .swiper_item {
-      position: relative;
-      width: 82.9439vw;
-      height: 128.7383vw;
-      border-radius: 25px;
-      box-shadow: 0px 3px 20px 1px rgba(4,44,110,0.16);
-      background: #fff url('@/assets/home/card_bg.png');
-      background-size: 100% auto;
-      .logos {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 3.5047vw 4.6729vw;
-        .logo_d9 {
-          width: 35.7477vw;
-          height: auto;
-        }
-        .logo_item {
-          width: 14.486vw;
-          height: 3.5047vw;
-          line-height: 3.5047vw;
-          border-radius: 9px;
-          background-color: #0065B2;
-          color: #fff;
-          font-size: 1.692vw;
-          text-align: center;
-        }
-      }
-      .wallet_detail {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-color: #fff;
-        padding: 7.0093vw 4.6729vw;
-        border-radius: 25px;
-        .wallet_text {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          .text_left {
-            font-size: 4.2056vw;
-            font-weight: bold;
-            display: flex;
-            align-items: center;
-            .view_pic {
-              width: 3.5047vw;
-              height: auto;
-              margin-left: 2.3364vw;
-            }
-          }
-          .text_right {
-            display: flex;
-            align-items: center;
-            color: #0065FF;
-            font-size: 2.8037vw;
-            .arrow_pic {
-              width: 1.1682vw;
-              height: auto;
-              margin-left: 1.1682vw;
-            }
-          }
-        }
-        .wallet_balance {
-          display: flex;
-          align-items: baseline;
-          font-weight: bold;
-          padding: 4.6729vw 0;
-          .balance_symbol {
-            font-size: 4.2056vw;
-          }
-          .balance_num {
-            font-size: 8.4112vw;
-          }
-        }
-      }
-    }
-  }
+  
 }
 </style>
