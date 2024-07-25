5f0e403e9f4e503b7ff9b662b0b6a3ae9b826e40
coderdly <1445109929@qq.com>
2024-07-25 17:17:07 +0800

    无

--- components.d.ts
+++ components.d.ts
@@ -9,15 +9,10 @@ declare module 'vue' {
   export interface GlobalComponents {
     HomeAcceptModal: typeof import('./src/components/home/homeAcceptModal.vue')['default']
     HomeSwiper: typeof import('./src/components/home/homeSwiper.vue')['default']
-    'HomeSwiper.vue.BASE': typeof import('./src/components/home/homeSwiper.vue.BASE.vue')['default']
-    'HomeSwiper.vue.LOCAL': typeof import('./src/components/home/homeSwiper.vue.LOCAL.vue')['default']
-    'HomeSwiper.vue.REMOTE': typeof import('./src/components/home/homeSwiper.vue.REMOTE.vue')['default']
     HomeTransferModal: typeof import('./src/components/home/homeTransferModal.vue')['default']
-    'HomeTransferModal.vue.BASE': typeof import('./src/components/home/homeTransferModal.vue.BASE.vue')['default']
-    'HomeTransferModal.vue.LOCAL': typeof import('./src/components/home/homeTransferModal.vue.LOCAL.vue')['default']
-    'HomeTransferModal.vue.REMOTE': typeof import('./src/components/home/homeTransferModal.vue.REMOTE.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    VanPopup: typeof import('vant/es')['Popup']
+    VarSwipe: typeof import('@varlet/ui')['_SwipeComponent']
+    VarSwipeItem: typeof import('@varlet/ui')['_SwipeItemComponent']
   }
 }
